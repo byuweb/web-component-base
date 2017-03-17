b7f8c59a3825bd6affcd0c44ff899c073a91fe22
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-03-17 15:53:32 -0600

    Implement applyTemplate helper for Shadow/Shady DOM template stamping

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ThatJoeMoore on 2/14/17
+ */
+"use strict";
+
+import applyTemplate from './utils/templating';
+
+export {
+    applyTemplate
+};
---
+++ utils/templating.js
@@ -0,0 +1,68 @@
+/*
+ *  @license
+ *    Copyright 2017 Brigham Young University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+"use strict";
+
+import hash from 'hash-sum';
+
+export default function applyTemplate(element, elementName, template, callback) {
+    let sum = hash(template);
+
+    let elSettings = element.__byu_webCommunity_components = element.__byu_webCommunity_components || {};
+    if (elSettings.templateHash === sum) {
+        //Nothing has changed in the element.  Don't replace the DOM, don't fire the callback.
+        return;
+    }
+    elSettings.templateHash = sum;
+
+    if (window.ShadyCSS) {
+        applyTemplateShady(element, elementName, template, callback, sum);
+    } else {
+        applyTemplateNative(element, template, callback);
+    }
+}
+
+function applyTemplateShady(element, elementName, template, callback, sum) {
+    let templateId = '__byu-custom-element-template_' + elementName + '_' + sum;
+    let templateElement = document.head.querySelector('template#' + templateId);
+    if (!templateElement) {
+        templateElement = document.createElement('template');
+        templateElement.id = templateId;
+        templateElement.innerHTML = template;
+        document.head.appendChild(templateElement);
+        ShadyCSS.prepareTemplate(templateElement, elementName);
+    }
+    ShadyCSS.styleElement(element);
+    let imported = document.importNode(templateElement.content, true);
+    let shadow = element.shadowRoot;
+    //It'd be nice if we could just diff the DOM and replace what changed between templates, but that might lead to
+    // event listeners getting applied twice.  Easier to just clear out the shadow DOM and replace it.
+    while (shadow.firstChild) {
+        shadow.removeChild(shadow.firstChild);
+    }
+    shadow.appendChild(imported);
+    if (callback) {
+        setTimeout(callback);
+    }
+}
+
+function applyTemplateNative(element, template, callback) {
+    element.shadowRoot.innerHTML = template;
+    if (callback) {
+        callback();
+    }
+}
+
